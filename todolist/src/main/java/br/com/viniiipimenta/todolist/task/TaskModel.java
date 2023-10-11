c5fdc22dc757c9b7ed7eecfa81dbdf0fbdf0b9b5
Marcus Vinicius Pimenta <57456066+viniiipimenta@users.noreply.github.com>
2023-10-11 17:43:49 -0300

    day 4
    created tasks and doing password authentication

---
+++ todolist/src/main/java/br/com/viniiipimenta/todolist/task/TaskModel.java
@@ -0,0 +1,33 @@
+package br.com.viniiipimenta.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_taks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID    id;
+    
+    @Column(length = 50)
+    private String  title;
+    private String  description;
+    private String  startAt;
+    private String  endAt;
+    private String  priority;
+
+    private UUID userId;
+
+    @CreationTimestamp
+    private LocalDateTime   createdAt;
+}
